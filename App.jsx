cd578fa131bfca50578370c6ddc2196d20822b00
Sanskar Sharma <143111025+sanskar-775@users.noreply.github.com>
2024-06-20 15:44:05 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,22 @@
+import { Route,createBrowserRouter,createRoutesFromElements,RouterProvider} from 'react-router-dom'
+
+
+import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
+import JobPage from './pages/JobPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout/>}> 
+      <Route index element={<HomePage/>}/>
+      <Route path='/jobs' element={<JobPage/>}/>
+      <Route path='*' element={<NotFoundPage/>}/>
+    </Route>)
+);
+
+const App = () => {
+  return <RouterProvider router={router}/>;
+};
+
+export default App
---
+++ HomePage.jsx
@@ -0,0 +1,19 @@
+
+import Hero from '../Components/Hero'
+import HomeCards from "../Components/HomeCards"
+import JobLIstings from "../Components/JobLIstings"
+import ViewAllJobs from "../Components/ViewAllJobs"
+
+
+const HomePage = () => {
+  return (
+    <>
+        <Hero/>
+        <HomeCards/>
+        <JobLIstings/>
+        <ViewAllJobs/>
+    </>
+  );
+};
+
+export default HomePage
